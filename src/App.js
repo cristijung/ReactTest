2879e333703cceb5efb6e458a68fb43c64264ae5
Cristina Jung <cristijung@gmail.com>
2022-08-27 00:43:48 -0300

    testes-final-aula

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 
-
 function App() {
   return (    
     <div className="App">
       <header className="App-header">
         <h1>Testes Unitários</h1>    
-        <h2>Organizando o arquivo de Testes</h2>        
-      </header>      
+        <h2>Organizando o arquivo de Testes</h2> 
+        <a href="https://www.google.com/" target="_blank" className='App-link'>Clique Aqui</a>   
+          <div>
+          <label  htmlFor='checkbox'>Verificar</label>
+          <input  id='checkbox'  type='checkbox'/>
+        </div> 
+      </header> 
     </div> 
    
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from  '@testing-library/user-event';
 import App from './App';
 
 describe('Componente principal', () => {
@@ -12,5 +13,16 @@ describe('Componente principal', () => {
     render (<App />);
     expect(screen.getByText('Organizando o arquivo de Testes')).toBeInTheDocument();
   })
-  
+
+  test('Verificar o texto do link e o link', () => {
+    render (<App />);
+    expect(screen.getByText('Clique Aqui').closest('a')).toHaveAttribute('href', 'https://www.google.com/');
+  })
+
+  test('Teste de renderização do CheckBox', () => {
+    render(<App />);   
+    userEvent.click(screen.getByText('Verificar'))   
+    expect(screen.getByLabelText('Verificar')).toBeChecked();   
+   })
+
 })
